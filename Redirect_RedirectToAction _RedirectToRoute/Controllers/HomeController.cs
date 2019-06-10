7bc5bdf01748aca828bb54fd20caede3ea4f16ec
KhahoryITLA <20164247@itla.edu.do>
2019-06-09 22:09:21 -0400

    Commit

---
+++ Redirect_RedirectToAction _RedirectToRoute/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Redirect_RedirectToAction__RedirectToRoute.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //retorna la direccion de una web
+            return Redirect("https://orbi.edu.do/orbi/#");
+            return View();
+        }
+
+        public RedirectToRouteResult About()
+        {
+            //retornar otra accion y otro controlador
+            ViewBag.Message = "Your application description page.";
+
+            return RedirectToAction("Contact","Home");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Redirect_RedirectToAction _RedirectToRoute/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Redirect_RedirectToAction__RedirectToRoute.MvcApplication" Language="C#" %>
